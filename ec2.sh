4b6391268a31ce721eae47e2eb37414d7cdb7b3f
Igor Gomes <i.greygomes3@outlook.com>
2022-05-06 14:42:10 -0300

    Create ec2.sh

---
+++ ec2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sudo passwd ubuntu 
+
+sudo apt update && sudo apt upgrade
+clear
+sudo apt install software-properties-common
+clear
+sudo apt-get install xrdp lxde-core lxde tigervnc-standalone-server -y
+clear
+
+sudo add-apt-repository ppa:linuxuprising/java  
+sudo apt update  
+sudo apt install oracle-java12-installer  
+
+java -version
+if [ $? -eq 0 ]
+then
+echo \"java instalado\"
+else
+echo \"java não instalado\"
+echo \"gostaria de instalar o java? S/n \"
+read inst
+if [ \"$inst\" == \"s\" ]
+then
+echo \"voce escolheu instalar o java\"
+echo \"instalado java\"
+sleep 2
+sudo apt install default-jre
+echo \"sua versão java é:\" & java-version
+fi
+fi
